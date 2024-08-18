771e11750c02219dc1f5d99a8658c863b7d29ca7
mathewkoech <mathewkoech55@gmail.com>
2024-08-18 09:48:12 +0000

    helper function to return tuple of page and page_size as a list

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 """
-Simple helper function return a tuple of size two containing a start index and an 
-end index corresponding to the range of indexes to return 
+Simple helper function return a tuple of size
+two containing a start index and an
+end index corresponding to the range of indexes to return
 in a list for those particular pagination parameters.
 """
 from typing import Tuple
