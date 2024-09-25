b215ec6e8b2dc4559b6c762aeb8c6c3f3b860808
mathewkoech <mathewkoech55@gmail.com>
2024-09-25 10:30:25 +0000

    job creation

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,26 +1,27 @@
-import kue from 'kue';
 
-function createPushNotificationsJobs(jobs, queue) {
-  if (!Array.isArray(jobs)) {
-    throw new Error('Jobs is not an array');
-  }
 
-  jobs.forEach((jobData) => {
-    const job = queue.create('push_notification_code_3', jobData)
-      .save((err) => {
-        if (!err) {
-          console.log(`Notification job created: ${job.id}`);
-        }
-      });
+export default function createPushNotificationsJobs(jobs, queue) {
+    if (!Array.isArray(jobs)) {
+        throw Error('Jobs is not an array');
+    }
+    jobs.forEach(job => {
+        const newJob = queue.create("push_notification_code_3", job)
+        newJob.save((err) => {
+            if (!err) {
+                console.log(`Notification job created: ${newJob.id}`)
+            }
+        })
 
-    job.on('complete', () => {
-      console.log(`Notification job ${job.id} completed`);
-    }).on('failed', (err) => {
-      console.log(`Notification job ${job.id} failed: ${err}`);
-    }).on('progress', (progress) => {
-      console.log(`Notification job ${job.id} ${progress}% complete`);
+        newJob.on("complete", () => {
+            console.log(`Notification job ${newJob.id} completed`)
+        })
+        
+        newJob.on("failed", (err) => {
+            console.log(`Notification job ${newJob.id} failed: ${err}`)
+        })
+
+        newJob.on("progress", (percentage) => {
+            console.log(`Notification job ${newJob.id} ${percentage}% complete`)
+        })
     });
-  });
 }
-
-export default createPushNotificationsJobs;
